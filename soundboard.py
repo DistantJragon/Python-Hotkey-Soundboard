56f5f79aad2dee26e47626bdfada928fe969541a
Jacob Goodwin <53794957+DistantJragon@users.noreply.github.com>
2021-03-09 23:43:59 -0500

    version 1.2
    Multiple streams of same sound can play at once

--- soundboard.py
+++ soundboard.py
@@ -9,13 +9,14 @@ stopAllSoundsHotkey = 'ctrl+.'
 delayBeforeRestartSound = 0.1
 chunk = 2048
 deviceIndex = 10
+streamsPerSoundEntry = 3
 
 portAudioInterface = pyaudio.PyAudio()
 soundEntryList = []
-class Entry:
-    def __init__(self, fileName, hotkey):
+
+class Stream:
+    def __init__(self, fileName):
         self.fileName = 'Sounds/' + fileName + '.wav'
-        self.hotkey = hotkey
         self.waveFile = wave.open(self.fileName, 'rb')
         self.stream = portAudioInterface.open(
             output_device_index = deviceIndex,
@@ -24,11 +25,10 @@ class Entry:
             rate = self.waveFile.getframerate(),
             output = True
         )
-        self.playSoundThread = threading.Thread(target=self.play_sound_entry)
+        self.timeAtLastPlay = time.time()
         self.isPlaying = False
         self.played = False
-        self.timeAtLastPlay = time.time()
-        soundEntryList.append(self)
+        self.playSoundThread = threading.Thread(target=self.play_sound_entry)
     def play_sound_entry(self):
         self.waveFile = wave.open(self.fileName, 'rb')
         data = self.waveFile.readframes(chunk)
@@ -37,27 +37,56 @@ class Entry:
             self.stream.write(data)
             if keyboard.is_pressed(stopAllSoundsHotkey):
                 break
-            if keyboard.is_pressed(self.hotkey) and time.time() - self.timeAtLastPlay >= delayBeforeRestartSound:
-                self.waveFile = wave.open(self.fileName, 'rb')
-                self.timeAtLastPlay = time.time()
         self.isPlaying = False
         self.played = True
 
+class Entry:
+    def __init__(self, fileName, hotkey):
+        self.hotkey = hotkey
+        self.streamList = []
+        self.allStreamsArePlaying = False
+        self.resetFirstStream = False
+        self.timeAtLastPlay = time.time()
+        for i in range(streamsPerSoundEntry):
+            self.streamList.append(Stream(fileName))
+        soundEntryList.append(self)
+    def streamWithEarliestPlay(self, streamList):
+        earliestStream = streamList[0]
+        for stream in streamList:
+            if stream.timeAtLastPlay < earliestStream.timeAtLastPlay:
+                earliestStream = stream
+        return earliestStream
+
 exampleSound = Entry('Ricardo boink', 'ctrl+num 1')
-exampleSound2 = Entry('no i didnt', 'ctrl+num 2')
+exampleSound2 = Entry('weiland Fuck. my. dick. off. gam. ers. eeeerrrww', 'ctrl+num 2')
 
 def detect_press_hotkey():
     while True:
-        time.sleep(0.1)
-        for entry in soundEntryList:
-            if entry.played:
-                entry.playSoundThread.join()
-                entry.played = False
-            if keyboard.is_pressed(entry.hotkey) and not entry.isPlaying and time.time() - entry.timeAtLastPlay >= delayBeforeRestartSound:
-                entry.playSoundThread = threading.Thread(target=entry.play_sound_entry)
-                entry.playSoundThread.start()
-                entry.isPlaying = True
-                entry.timeAtLastPlay = time.time()
+        time.sleep(0.05)
+        for soundEntry in soundEntryList:
+            for stream in soundEntry.streamList:
+                if stream.played:
+                    stream.playSoundThread.join()
+                    stream.played = False
+                    soundEntry.allStreamsArePlaying = False
+            if keyboard.is_pressed(soundEntry.hotkey) and time.time() - soundEntry.timeAtLastPlay >= delayBeforeRestartSound:
+                for stream in soundEntry.streamList:
+                    if not stream.isPlaying:  
+                        stream.playSoundThread = threading.Thread(target=stream.play_sound_entry)
+                        stream.playSoundThread.start()
+                        stream.isPlaying = True
+                        soundEntry.timeAtLastPlay = time.time()
+                        stream.timeAtLastPlay = time.time()
+                        print(1)
+                        break
+                    else:
+                        soundEntry.allStreamsArePlaying = True
+                if soundEntry.allStreamsArePlaying and time.time() - soundEntry.timeAtLastPlay >= delayBeforeRestartSound:
+                    print(2)
+                    soundEntry.streamWithEarliestPlay(soundEntry.streamList).waveFile = wave.open(stream.fileName, 'rb')
+                    soundEntry.streamWithEarliestPlay(soundEntry.streamList).timeAtLastPlay = time.time()
+                    soundEntry.timeAtLastPlay = time.time()
+
                 
 mainThread = threading.Thread(target=detect_press_hotkey)
 mainThread.start()
