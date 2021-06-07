8bdcb6e24a5889f1c5f35d7b0575071d9de14c4d
Jacob Goodwin <53794957+DistantJragon@users.noreply.github.com>
2021-06-07 15:53:05 -0400

    Grouping Sounds and JSON
    Uses JSON to allow users to add sound entries more immediately and without going into the python file. going into the python file is now only necessary for changing settings. Sounds can be grouped now, allowing more customization.

--- soundboard.py
+++ soundboard.py
@@ -1,34 +1,29 @@
 import pyaudio
 import wave
-import sys
 import keyboard
 import threading
 import time
 import os
 import math
 import random
+import json
 
 # Options
+makeGroupWithAllSounds = False # Makes a sound entry using every sound in the Sounds folder that plays in a random order. Default: False
+allSoundsGroupHotkey = 'ctrl+num /' # Default: 'ctrl+num /'
 stopAllSoundsHotkey = 'ctrl+.' # Default: 'ctrl+.'
 delayBeforeRestartSound = 0.1 # Prevents rapid-fire playing of sounds. Default: 0.1
-chunk = 2048 # As I understand it, a buffer for pyAudio. This program defaults in to 2048. PyAudio defaulted it to 1024.
+chunk = 2048 # As I understand it, a buffer for pyAudio. This program defaults it to 2048. PyAudio defaulted it to 1024.
 deviceIndex = 10 # Find the Device ID of your speakers or your Virtual Audio Cable with seeIndeciesOfDevices.py (Probably required). Default: 10
 streamsPerSoundEntry = 3 # How many times the same sound can overlap itself. Default: 3
 stopAllSoundsWithNewSound = False # Stops all sounds if a new sound plays. Default: False
 
-soundFiles = [fileName for fileName in os.listdir('Sounds')]
-for i in range(len(soundFiles)):
-	if not '.wav' in soundFiles[i]:
-		soundFiles.pop(i)
-		continue
-	soundFiles[i] = soundFiles[i][:-4]
-
+# Code
 def randomInteger(min, max):
 	x = math.floor(random.random() * (max - min + 1) + min)
 	return x
 
 portAudioInterface = pyaudio.PyAudio()
-soundEntryList = []
 class Stream:
 	def __init__(self, fileName, parent):
 		self.parent = parent
@@ -55,16 +50,17 @@ class Stream:
 		self.parent.allStreamsArePlaying = False
 		return
 
+soundEntryList = {}
 class Entry:
-	def __init__(self, fileName, hotkey):
-		self.hotkey = hotkey
+	def __init__(self, fileName):
+		global soundEntryList
 		self.fileName = 'Sounds/' + fileName + '.wav'
 		self.streamList = []
 		self.allStreamsArePlaying = False
 		self.timeAtLastPlay = time.time()
-		for i in range(streamsPerSoundEntry):
+		while len(self.streamList) < streamsPerSoundEntry:
 			self.streamList.append(Stream(fileName, self))
-		soundEntryList.append(self)
+		soundEntryList[fileName] = self
 	def streamWithEarliestPlay(self):
 		earliestStream = self.streamList[0]
 		for stream in self.streamList:
@@ -72,10 +68,42 @@ class Entry:
 				earliestStream = stream
 		return earliestStream
 
-# Add new sounds here
-exampleSound = Entry('Ricardo boink', 'ctrl+num 1')
-exampleSound2 = Entry('no i didnt', 'ctrl+num 2')
-randomSound = Entry(soundFiles[randomInteger(0, len(soundFiles) - 1)], "ctrl+num /")
+def getAllSoundFileNames():
+	soundFilesNames = [fileName for fileName in os.listdir('Sounds')]
+	for i in range(len(soundFilesNames)):
+		if not '.wav' in soundFilesNames[i]:
+			soundFilesNames.pop(i)
+			continue
+		soundFilesNames[i] = soundFilesNames[i][:-4]
+	return soundFilesNames
+
+groupList = []
+
+if makeGroupWithAllSounds:
+	soundFilesNames = getAllSoundFileNames()
+	allSoundsDictionary = {'playRandomly':True, 'hotkeys':[allSoundsGroupHotkey]}
+	allSoundsDictionary['sounds'] = []
+	for sound in soundFilesNames:
+		allSoundsDictionary['sounds'].append({'name':sound, 'weight':1})
+	groupList.append(allSoundsDictionary)
+	for sound in soundFilesNames:
+		Entry(sound)
+
+jsonFile = open('groupList.json')
+jsonData = json.load(jsonFile)
+groupEntries = jsonData['groupEntries']
+groupEntriesKeys = groupEntries.keys()
+for key in groupEntriesKeys:
+	groupList.append(groupEntries[key])
+	if not makeGroupWithAllSounds:
+		for sound in groupEntries[key]['sounds']:
+			Entry(sound['name'])
+
+for group in groupList:
+	group['weightSum'] = 0
+	for sound in group['sounds']:
+		group['weightSum'] += sound['weight']
+	group['orderTracker'] = 0
 
 currentSoundPlaying = ''
 
@@ -83,8 +111,6 @@ def playSound(soundEntry):
 	global currentSoundPlaying
 	if time.time() - soundEntry.timeAtLastPlay <= delayBeforeRestartSound:
 		return
-	if randomSound:
-		randomSound.fileName = 'Sounds/' + soundFiles[randomInteger(0, len(soundFiles) - 1)] + '.wav'
 	currentSoundPlaying = soundEntry
 	soundEntry.timeAtLastPlay = time.time()
 	soundEntry.allStreamsArePlaying = True
@@ -103,8 +129,25 @@ def playSound(soundEntry):
 		earliestStream.waveFile = wave.open(soundEntry.fileName, 'rb')
 		earliestStream.timeAtLastPlay = time.time()
 	
-for soundEntry in soundEntryList:
-	keyboard.add_hotkey(soundEntry.hotkey, playSound, args=(soundEntry,), timeout=delayBeforeRestartSound)
+def playSoundGroup(soundGroup):
+	if soundGroup['playRandomly']:
+		randomNumber = random.random() * soundGroup['weightSum']
+		for sound in soundGroup['sounds']:
+			if randomNumber <= sound['weight']:
+				playSound(soundEntryList[sound['name']])
+				break
+			else:
+				randomNumber -= sound['weight']
+	else:
+		soundInFrontOfLine = soundGroup['sounds'][soundGroup['orderTracker']]
+		playSound(soundEntryList[soundInFrontOfLine['name']])
+		soundGroup['orderTracker'] += 1
+		if soundGroup['orderTracker'] > len(soundGroup['sounds']) - 1:
+			soundGroup['orderTracker'] = 0
+
+for group in groupList:
+	for hotkey in group['hotkeys']:
+		keyboard.add_hotkey(hotkey, playSoundGroup, args=(group,), timeout=delayBeforeRestartSound)
 
 def keepProgramRunning():
 	while True:
