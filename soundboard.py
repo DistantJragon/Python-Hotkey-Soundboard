fb493793a76217cb5fb58a2c47b04a2303d9032d
Jacob Goodwin <53794957+DistantJragon@users.noreply.github.com>
2022-05-08 17:59:23 -0400

    Pycharm is angry

--- soundboard.py
+++ soundboard.py
@@ -8,94 +8,105 @@ import math
 import random
 import json
 
-# Options
-jsonFile = open('optionsList.json')
-jsonData = json.load(jsonFile)
-options = jsonData['options']
-makeGroupWithAllSounds = options['makeGroupWithAllSounds']['state'] # Makes a sound entry using every sound in the Sounds folder that plays in a random order. Default: False
-allSoundsGroupHotkey = options['allSoundsGroupHotkey']['state'] # Default: 'ctrl+num /'
-stopAllSoundsHotkey = options['stopAllSoundsHotkey']['state'] # Default: 'ctrl+.'
-delayBeforeRestartSound = options['delayBeforeRestartSound']['state'] # Prevents rapid-fire playing of sounds. Default: 0.1
-chunk = options['chunk']['state'] # As I understand it, a buffer for pyAudio. This program defaults it to 2048. PyAudio defaulted it to 1024.
-deviceIndex = options['deviceIndex']['state'] # Find the Device ID of your speakers or your Virtual Audio Cable with seeIndeciesOfDevices.py (Probably required). Default: 10
-streamsPerSoundEntry = options['streamsPerSoundEntry']['state'] # How many times the same sound can overlap itself. Default: 3
-stopAllSoundsWithNewSound = options['stopAllSoundsWithNewSound']['state'] # Stops all sounds if a new sound plays. Default: False
+optionsFile = open('optionsList.json')
+optionsData = json.load(optionsFile)
+options = optionsData['options']
+makeGroupWithAllSounds = options['makeGroupWithAllSounds']['state']
+allSoundsGroupHotkey = options['allSoundsGroupHotkey']['state']
+stopAllSoundsHotkey = options['stopAllSoundsHotkey']['state']
+delayBeforeRestartSound = options['delayBeforeRestartSound']['state']
+chunk = options['chunk']['state']
+deviceIndex = options['deviceIndex']['state']
+streamsPerSoundEntry = options['streamsPerSoundEntry']['state']
+stopAllSoundsWithNewSound = options['stopAllSoundsWithNewSound']['state']
 
-# Code
-def randomInteger(min, max):
-	x = math.floor(random.random() * (max - min + 1) + min)
+
+def random_integer(min_range, max_range):
+	x = math.floor(random.random() * (max_range - min_range + 1) + min_range)
 	return x
 
+
 portAudioInterface = pyaudio.PyAudio()
-print("Device ID ", deviceIndex, " - ", portAudioInterface.get_device_info_by_host_api_device_index(0, deviceIndex).get('name'))
+currentDeviceName = portAudioInterface.get_device_info_by_host_api_device_index(0, deviceIndex).get('name')
+print("Device ID ", deviceIndex, " - ", currentDeviceName)
+
+
 class Stream:
-	def __init__(self, fileName, parent):
+	def __init__(self, file_name, parent):
 		self.parent = parent
-		self.waveFile = wave.open(self.parent.fileName, 'rb')
+		self.waveFile = wave.open(file_name, 'rb')
 		self.stream = portAudioInterface.open(
-			output_device_index = deviceIndex,
-			format = portAudioInterface.get_format_from_width(self.waveFile.getsampwidth()),
-			channels = self.waveFile.getnchannels(),
-			rate = self.waveFile.getframerate(),
-			output = True
+			output_device_index=deviceIndex,
+			format=portAudioInterface.get_format_from_width(self.waveFile.getsampwidth()),
+			channels=self.waveFile.getnchannels(),
+			rate=self.waveFile.getframerate(),
+			output=True
 		)
 		self.timeAtLastPlay = time.time()
 		self.isPlaying = False
 		self.playSoundThread = threading.Thread(target=self.play_sound_entry)
+
 	def play_sound_entry(self):
 		self.waveFile = wave.open(self.parent.fileName, 'rb')
 		data = self.waveFile.readframes(chunk)
 		while data:
 			data = self.waveFile.readframes(chunk)
 			self.stream.write(data)
-			if keyboard.is_pressed(stopAllSoundsHotkey) or (stopAllSoundsWithNewSound and self.parent is not currentSoundPlaying):
+			stop_sound = keyboard.is_pressed(stopAllSoundsHotkey)
+			stop_sound = stop_sound or (stopAllSoundsWithNewSound and self.parent is not currentSoundPlaying)
+			if stop_sound:
 				break
 		self.isPlaying = False
 		self.parent.allStreamsArePlaying = False
 		return
 
+
 soundEntryList = {}
+
+
 class Entry:
-	def __init__(self, fileName, numberOfStreams):
+	def __init__(self, file_name, number_of_streams):
 		global soundEntryList
-		self.fileName = 'Sounds/' + fileName + '.wav'
+		self.fileName = 'Sounds/' + file_name + '.wav'
 		self.streamList = []
 		self.allStreamsArePlaying = False
 		self.timeAtLastPlay = time.time()
-		while len(self.streamList) < numberOfStreams:
-			self.streamList.append(Stream(fileName, self))
-		soundEntryList[fileName] = self
-	def streamWithEarliestPlay(self):
-		earliestStream = self.streamList[0]
-		for stream in self.streamList:
-			if stream.timeAtLastPlay < earliestStream.timeAtLastPlay:
-				earliestStream = stream
-		return earliestStream
+		while len(self.streamList) < number_of_streams:
+			self.streamList.append(Stream(file_name, self))
+		soundEntryList[file_name] = self
 
-def getAllSoundFileNames():
-	soundFilesNames = [fileName for fileName in os.listdir('Sounds')]
-	for i in range(len(soundFilesNames)):
-		if not '.wav' in soundFilesNames[i]:
-			soundFilesNames.pop(i)
+	def stream_with_earliest_play(self):
+		earliest_stream = self.streamList[0]
+		for stream in self.streamList:
+			if stream.timeAtLastPlay < earliest_stream.timeAtLastPlay:
+				earliest_stream = stream
+		return earliest_stream
+
+
+def get_all_sound_file_names():
+	sound_files_names = [fileName for fileName in os.listdir('Sounds')]
+	for i in range(len(sound_files_names)):
+		if '.wav' not in sound_files_names[i]:
+			sound_files_names.pop(i)
 			continue
-		soundFilesNames[i] = soundFilesNames[i][:-4]
-	return soundFilesNames
+		sound_files_names[i] = sound_files_names[i][:-4]
+	return sound_files_names
+
 
 groupList = []
 
 if makeGroupWithAllSounds:
-	soundFilesNames = getAllSoundFileNames()
-	allSoundsDictionary = {'playRandomly':True, 'hotkeys':[allSoundsGroupHotkey], 'numberOfStreams':1}
-	allSoundsDictionary['sounds'] = []
+	soundFilesNames = get_all_sound_file_names()
+	allSoundsDictionary = {'playRandomly': True, 'hotkeys': [allSoundsGroupHotkey], 'numberOfStreams': 1, 'sounds': []}
 	for sound in soundFilesNames:
-		allSoundsDictionary['sounds'].append({'name':sound, 'weight':1})
+		allSoundsDictionary['sounds'].append({'name': sound, 'weight': 1})
 	groupList.append(allSoundsDictionary)
 	for sound in soundFilesNames:
 		Entry(sound, 1)
 
-jsonFile = open('groupList.json')
-jsonData = json.load(jsonFile)
-groupEntries = jsonData['groupEntries']
+groupFile = open('groupList.json')
+groupData = json.load(groupFile)
+groupEntries = groupData['groupEntries']
 groupEntriesKeys = groupEntries.keys()
 for key in groupEntriesKeys:
 	groupList.append(groupEntries[key])
@@ -111,52 +122,57 @@ for group in groupList:
 
 currentSoundPlaying = ''
 
-def playSound(soundEntry):
+
+def play_sound(sound_entry):
 	global currentSoundPlaying
-	if time.time() - soundEntry.timeAtLastPlay <= delayBeforeRestartSound:
+	if time.time() - sound_entry.timeAtLastPlay <= delayBeforeRestartSound:
 		return
-	currentSoundPlaying = soundEntry
-	soundEntry.timeAtLastPlay = time.time()
-	soundEntry.allStreamsArePlaying = True
-	for stream in soundEntry.streamList:
+	currentSoundPlaying = sound_entry
+	sound_entry.timeAtLastPlay = time.time()
+	sound_entry.allStreamsArePlaying = True
+	for stream in sound_entry.streamList:
 		if not stream.isPlaying:
 			stream.playSoundThread = threading.Thread(target=stream.play_sound_entry)
 			stream.playSoundThread.start()
 			stream.isPlaying = True
 			stream.timeAtLastPlay = time.time()
-			print("Playing " + soundEntry.fileName)
-			soundEntry.allStreamsArePlaying = False
+			print("Playing " + sound_entry.fileName)
+			sound_entry.allStreamsArePlaying = False
 			break
-	earliestStream = soundEntry.streamWithEarliestPlay()
-	if soundEntry.allStreamsArePlaying:
-		print("Playing " + soundEntry.fileName)
-		earliestStream.waveFile = wave.open(soundEntry.fileName, 'rb')
-		earliestStream.timeAtLastPlay = time.time()
-	
-def playSoundGroup(soundGroup):
-	if soundGroup['playRandomly']:
-		randomNumber = random.random() * soundGroup['weightSum']
-		for sound in soundGroup['sounds']:
-			if randomNumber <= sound['weight']:
-				playSound(soundEntryList[sound['name']])
+	earliest_stream = sound_entry.stream_with_earliest_play()
+	if sound_entry.allStreamsArePlaying:
+		print("Playing " + sound_entry.fileName)
+		earliest_stream.waveFile = wave.open(sound_entry.fileName, 'rb')
+		earliest_stream.timeAtLastPlay = time.time()
+
+
+def play_sound_group(sound_group):
+	if sound_group['playRandomly']:
+		random_number = random.random() * sound_group['weightSum']
+		for current_sound in sound_group['sounds']:
+			if random_number <= current_sound['weight']:
+				play_sound(soundEntryList[current_sound['name']])
 				break
 			else:
-				randomNumber -= sound['weight']
+				random_number -= current_sound['weight']
 	else:
-		soundInFrontOfLine = soundGroup['sounds'][soundGroup['orderTracker']]
-		playSound(soundEntryList[soundInFrontOfLine['name']])
-		soundGroup['orderTracker'] += 1
-		if soundGroup['orderTracker'] > len(soundGroup['sounds']) - 1:
-			soundGroup['orderTracker'] = 0
+		sound_in_front_of_line = sound_group['sounds'][sound_group['orderTracker']]
+		play_sound(soundEntryList[sound_in_front_of_line['name']])
+		sound_group['orderTracker'] += 1
+		if sound_group['orderTracker'] > len(sound_group['sounds']) - 1:
+			sound_group['orderTracker'] = 0
+
 
 for group in groupList:
 	for hotkey in group['hotkeys']:
-		keyboard.add_hotkey(hotkey, playSoundGroup, args=(group,), timeout=delayBeforeRestartSound)
+		keyboard.add_hotkey(hotkey, play_sound_group, args=(group,), timeout=delayBeforeRestartSound)
 
-def keepProgramRunning():
+
+def keep_program_running():
 	while True:
 		time.sleep(1)
-keepRunningThread = threading.Thread(target=keepProgramRunning)
-keepRunningThread.start()
 
+
+keepRunningThread = threading.Thread(target=keep_program_running)
+keepRunningThread.start()
 print('Ready!')
